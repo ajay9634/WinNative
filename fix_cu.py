79e4e8fd108e5986af9d52ec8137d49dfec1aaef
maxjivi05 <maxjivi05@users.noreply.github.com>
2026-03-06 11:55:49 -0500

    Remove Python migration tools from tracking and add to .gitignore

--- fix_cu.py
+++
@@ -1,8 +0,0 @@
-path = 'app/src/main/java/com/winlator/cmod/steam/utils/ContainerUtils.kt'
-with open(path, 'r') as f:
-    content = f.read()
-
-content = content.replace('null /*', 'null //')
-
-with open(path, 'w') as f:
-    f.write(content)
--- fix_db_export.py
+++
@@ -1,8 +0,0 @@
-path = 'app/src/main/java/com/winlator/cmod/steam/db/PluviaDatabase.kt'
-with open(path, 'r') as f:
-    content = f.read()
-
-content = content.replace('exportSchema = true', 'exportSchema = false')
-
-with open(path, 'w') as f:
-    f.write(content)
